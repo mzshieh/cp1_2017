646db42cd1f1adb6f2dba9c64f84cac76edffc20
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2017-01-17 16:59:55 +0800

    some problem

---
+++ week01/PC_sqrt/GC.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from random import randint, random
+
+print(500)
+for _ in range(500):
+    print(randint(1,1048576))
---
+++ week01/PC_sqrt/PC.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import math
+
+for _ in range(int(input())):
+    n = int(input())
+    print(n-round(math.sqrt(n)))

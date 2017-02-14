ac4d77c92d647615c24f75996a836a954879d12c
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2017-02-14 13:44:31 +0800

    fix

--- week01/PA_inner_prod/gen.py
+++ week01/PA_inner_prod/gen.py
@@ -8,6 +8,11 @@ def two_vec(n):
         line.append(str(randint(0,(1<<32)-1)))
         line.append(' '*randint(0,4))
     print(' '.join(line))
+    line = []
+    for _ in range(n):
+        line.append(str(randint(0,(1<<32)-1)))
+        line.append(' '*randint(0,4))
+    print(' '.join(line))
 
 for _ in range(500):
     two_vec(randint(1,100))
